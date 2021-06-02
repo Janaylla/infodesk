c27b55bc751a2aedd23f0a449ef5f5e402ae7cfa
Janaylla Firmino <janayllaf@gmail.com>
2021-06-02 09:29:55 -0300

    Menu com rotas

--- infodesk/src/components/Header/Header.js
+++ infodesk/src/components/Header/Header.js
@@ -4,25 +4,28 @@ import {Nav} from '../../GlobalStyle'
 import { ReactComponent as Logo } from '../../assets/logo/Image1.svg'
 import { ArrowDropDown, Search } from '@material-ui/icons'
 import Button from '../../components/Button/Button'
+import {goToAbout, goToHome, goToVideos, goToFind} from '../../Routes/Coordinators'
+import {useHistory} from 'react-router-dom'
 export default function Header() {
+  const history = useHistory()
   return (
     <Main>
       <Left>
-        <Logo className="logo" />
+        <Logo className="logo" onClick={() => goToHome(history)}/>
         <Nav>
           <ul >
-            <li>
+            <li onClick={() => goToAbout(history)}>
               About
               <ArrowDropDown />
             </li>
-            <li>
+            <li onClick={() => goToFind(history)}>
               Get informed about
               <ArrowDropDown />
             </li>
             <li>
               Stories & cases
             </li>
-            <li>
+            <li  onClick={() => goToVideos(history)}>
             News & videos
             <ArrowDropDown />
             </li>
--- infodesk/src/components/SectionVideo/SectionVideo.js
+++ infodesk/src/components/SectionVideo/SectionVideo.js
@@ -4,15 +4,16 @@ import {AccessTime} from '@material-ui/icons'
 import PostComment from '../../components/PostComment/PostComment'
 import Coment from '../../components/Comment/CardComment'
 const SectionVideo = () => {
+    
     return (
         <DivSection>
             <video src="https://storage.googleapis.com/future-apis.appspot.com/1.mp4" controls autoplay></video>
             <Title>
                 <h2>Title </h2>
-                <h3><AccessTime/>Maio, 2021</h3>
+                <h3><AccessTime/>May, 2021</h3>
             </Title>
             <PostComment/>
-           <Coment name="Felipe" text="Reliable ande relevant information.
+           <Coment name="Felipe" text="Reliable and relevant information.
 Let us help you start your future" like/>
         </DivSection>
     )
